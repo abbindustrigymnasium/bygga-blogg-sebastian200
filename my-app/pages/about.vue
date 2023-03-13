c4f60c65d37a08b5521b0636e255fa52210677e4
sebastian200 <kotte.alin@gmail.com>
2023-03-13 13:13:49 +0100

    about us page

---
+++ my-app/pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2> about</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci amet quam molestiae perferendis vitae, rerum sed dolorem ea fugit. Quidem excepturi dolore maiores minus quod nobis amet sed atque beatae.</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas fuga doloremque alias soluta quasi! Libero unde, minus quisquam quibusdam ad explicabo illo eligendi. Tenetur nostrum dignissimos natus saepe quidem modi!</p>
+
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+ h2 {
+    margin-bottom: 20px;
+    font-size: 36px;
+ }
+ p {
+    margin: 20px 0;
+ }
+
+</style>
