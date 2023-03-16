1777a6d049fdd92e47259bbd22225e3d25ba0641
sebastian200 <kotte.alin@gmail.com>
2023-03-16 17:14:00 +0100

    nuxt js product page

---
+++ my-app/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <header class="shadow-sm bg-white">
+            <nav class="container mx-auto p-6 flex justify-between">
+               <nuxt-link to="/" class="font-bold text-xl">test</nuxt-link>
+             <ul class="flex gap-4 text-xl">
+                <li><nuxt-link to="/products/hello" class="hover:text-purple-600">hello</nuxt-link></li>
+                <li><nuxt-link to="/products/hello" class="btn">hello</nuxt-link></li>
+                <li><nuxt-link to="/products" class="btn">products</nuxt-link></li>
+             </ul> 
+            </nav>
+
+
+        </header>
+        <div class="container mx-auto p-4" >
+            <slot />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ my-app/pages/products/[id].vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card h-screen">
+        <div>
+        <img :src="product.image" class="thumb" >
+        </div>
+        <p class="products">{{product.title}}</p>
+        <p class="products">{{product.price}}</p>
+        <p>{{product.id}}</p>
+
+
+    </div>
+
+
+</template>
+
+<script setup>
+
+    const {id} = useRoute().params
+
+    const uri = 'https://fakestoreapi.com/products/' + id;
+
+    const {data: product} = await useFetch(uri);
+
+
+</script>
+
+<style scoped>
+
+    .thumb {
+        max-height: 120px;
+        max-width: 70%;
+        margin: 0 auto;
+    }
+    
+</style>
