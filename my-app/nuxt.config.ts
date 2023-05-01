96ef8eaa6d65bb91ea3c6b62d06c2889ac6c026a
sebastian200 <kotte.alin@gmail.com>
2023-05-01 11:08:08 +0200

    last commit

--- my-app/nuxt.config.ts
+++ my-app/nuxt.config.ts
@@ -1,4 +1,4 @@
-export default defineNuxtConfig({
+export default {
     modules: [
         '@nuxt/content',
         '@nuxtjs/tailwindcss'
@@ -28,4 +28,4 @@ export default defineNuxtConfig({
         },
     },
 
-})
+}
